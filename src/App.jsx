7f36fd6c26bffb706929d7eab29959cc8268a4ee
Artem_Mentolos <mentolosscg@yandex.ru>
2023-09-09 13:17:51 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <span className="announce">Здесь скоро будет Skypro.Music</span>
+    </div>
+  );
+}
+
+export default App;
